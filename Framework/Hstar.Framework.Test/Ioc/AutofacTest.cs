2ed74628faaa23e7a4bc3818884e69d6571e11e7
hstarorg <HUmin910705>
2014-03-15 21:23:32 +0800

    提交人：Hstar 提交时间：2014-3-15 21:20:55 说明： 1、增加基础框架项目； 2、增加基础框架测试项目； 3、引入依赖注入框架Autofac，并封装为Helper类； 4、对AutofacHelper进行简单的单元测试；

---
+++ Framework/Hstar.Framework.Test/Ioc/AutofacTest.cs
@@ -0,0 +1,51 @@
+﻿using Hstar.Framework.Ioc;
+using NUnit.Framework;
+
+namespace Hstar.Framework.Test.Ioc
+{
+    internal class AutofacTest
+    {
+        /// <summary>
+        /// 测试采用默认配置文件时的情况
+        /// </summary>
+        [TestCase]
+        public void TestCase1()
+        {
+            AutofacHelper.InitAutofacContainer();
+            var fun = AutofacHelper.GetInstance<IFun>();
+            Assert.AreEqual("Fun1", fun.GetInstanceType());
+        }
+
+        /// <summary>
+        /// 测试独立配置文件时的情况
+        /// </summary>
+        [TestCase]
+        public void TestCase2()
+        {
+            AutofacHelper.InitAutofacContainer("../../Ioc/Autofac.xml");
+            var fun = AutofacHelper.GetInstance<IFun>();
+            Assert.AreEqual("Fun2", fun.GetInstanceType());
+        }
+    }
+
+    internal interface IFun
+    {
+        string GetInstanceType();
+    }
+
+    internal class Fun1 : IFun
+    {
+        public string GetInstanceType()
+        {
+            return "Fun1";
+        }
+    }
+
+    internal class Fun2 : IFun
+    {
+        public string GetInstanceType()
+        {
+            return "Fun2";
+        }
+    }
+}
---
+++ Framework/Hstar.Framework/Ioc/AutofacHelper.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Configuration;
+
+namespace Hstar.Framework.Ioc
+{
+    public sealed class AutofacHelper
+    {
+        /// <summary>
+        /// 定义静态的Ioc容器对象
+        /// </summary>
+        private static IContainer container;
+
+        /// <summary>
+        /// 通过配置文件，初始化Ioc容器的配置
+        /// </summary>
+        /// <param name="configFilePath">配置文件地址</param>
+        public static void InitAutofacContainer(string configFilePath = null)
+        {
+            var containerBuilder = new ContainerBuilder();
+            //从指定的xml配置文件中，读取Ioc依赖配置（如果未指定配置文件，那么从App.config/Web.config中读取）
+            containerBuilder.RegisterModule(configFilePath == null ? new ConfigurationSettingsReader("autofac") : new ConfigurationSettingsReader("autofac", configFilePath));
+            //初始化Ioc容器
+            container = containerBuilder.Build();
+        }
+
+        /// <summary>
+        /// 返回对象的实例
+        /// </summary>
+        /// <typeparam name="T">类型T</typeparam>
+        /// <returns></returns>
+        public static T GetInstance<T>() where T : class
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
