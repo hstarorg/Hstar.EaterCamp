e3d70ea9640cc3586df10b784fd91a8406f53cab
Hstar <hm910705@163.com>
2014-06-23 22:57:44 +0800

    提交人：Hstar 描述： 1、项目基架完善；

--- EaterCamp/Hstar.EaterCamp.WebUI/App_Start/BundleConfig.cs
+++ EaterCamp/Hstar.EaterCamp.WebUI/App_Start/BundleConfig.cs
@@ -7,16 +7,23 @@ namespace Hstar.EaterCamp.WebUI
         // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //jquery插件
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/Plugins/jQuery/jquery-{version}.js"));
+            //bootstrap插件
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/Plugins/bootstrap/js/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Scripts/Plugins/bootstrap/css/css").Include(
+                "~/Scripts/Plugins/bootstrap/css/bootstrap.css",
+                "~/Scripts/Plugins/bootstrap/css/bootstrap-theme.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/Plugins/jQueryUI/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/Plugins/jQueryValidate/jquery.unobtrusive*",
-                         "~/Scripts/Plugins/jQueryAjax/jquery.unobtrusive*",
-                        "~/Scripts/Plugins/jquery.validate*"));
+                "~/Scripts/Plugins/jquery.validate*",
+                "~/Scripts/Plugins/jQueryValidate/jquery.unobtrusive*",
+                "~/Scripts/Plugins/jQueryAjax/jquery.unobtrusive*"));
 
             // 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
